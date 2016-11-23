462010bdb65b45e765e8acdc17011891bdc91e5b
ZippoLee12138 <kulou322@gmail.com>
2016-11-23 23:01:35 +0800

    Initial commit

---
+++ CWNetwork/CWNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CWNetwork
+//
+//  Created by lzb on 2016/11/23.
+//  Copyright © 2016年 lzb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CWNetwork/CWNetwork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CWNetwork
+//
+//  Created by lzb on 2016/11/23.
+//  Copyright © 2016年 lzb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
